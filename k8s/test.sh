5b077784b2f7889c5153b30455c3e206eba477d6
Charles Moulliard <cmoulliard@redhat.com>
2023-06-22 09:39:59 +0200

    Refactoring the project to use functions and implemented new test case: Bearer auth
    Signed-off-by: Charles Moulliard <cmoulliard@redhat.com>

--- k8s/test.sh
+++ k8s/test.sh
@@ -3,23 +3,8 @@ LIFECYCLE_IMAGE=buildpacksio/lifecycle:0.16.3
 RUN_IMAGE=paketobuildpacks/run:tiny
 IMAGE_NAME=image-registry.openshift-image-registry.svc:5000/quarkus-hello
 
-cat <<EOF > auth.json
-{"image-registry.openshift-image-registry.svc:5000":"Basic $(echo "kubeadmin:$(oc whoami -t)" | base64)"}
-EOF
-kubectl delete cm registry-creds;kubectl create cm registry-creds --from-file=auth.json
-
-echo "############################################################################"
-echo "Test using Basic auth on ocp - https://github.com/buildpacks/spec/issues/277#issuecomment-992900561"
-echo "Registry: image-registry.openshift-image-registry.svc:5000"
-echo "Username: kubeadmin"
-echo "Password: $(oc whoami -t)"
-echo "############################################################################"
-
-kubectl delete -f https://raw.githubusercontent.com/redhat-buildpacks/testing/7615593bf80940f8410335decc9eccf6d9eeca18/k8s/tekton/buildpacks-phases.yml
-kubectl apply -f https://raw.githubusercontent.com/redhat-buildpacks/testing/7615593bf80940f8410335decc9eccf6d9eeca18/k8s/tekton/buildpacks-phases.yml
-kubectl delete PipelineRun/buildpacks-phases
-kubectl delete pvc/ws-pvc
-cat <<EOF | kubectl apply -f -
+function generatePipelineRun() {
+    cat <<EOF | kubectl apply -f -
 ---
 apiVersion: v1
 kind: PersistentVolumeClaim
@@ -91,27 +76,70 @@ spec:
       persistentVolumeClaim:
         claimName: ws-pvc
 EOF
+}
+function basicAuth() {
+  cat <<EOF > auth.json
+{"image-registry.openshift-image-registry.svc:5000":"Basic $(echo "kubeadmin:$(oc whoami -t)" | base64)"}
+EOF
+  kubectl delete cm registry-creds; kubectl create cm registry-creds --from-file=auth.json
 
-# Loop until the TaskRun exists
-while [[ -z "$(kubectl get -n default taskrun buildpacks-phases-buildpacks -o json)" ]]; do
-    echo "Waiting for TaskRun buildpacks-phases-buildpacks to exist..."
-    sleep 10
-done
+  echo "############################################################################"
+  echo "Test using Basic auth on ocp - https://github.com/buildpacks/spec/issues/277#issuecomment-992900561"
+  echo "Registry: image-registry.openshift-image-registry.svc:5000"
+  echo "Username: kubeadmin"
+  echo "Password: $(oc whoami -t)"
+  echo "############################################################################"
+}
 
-# Loop until the status is not null
-while [[ -z "$(tkn taskrun describe buildpacks-phases-buildpacks -ojson | jq '.status.steps[] | select(.container == "step-analyze" and has("terminated"))')" ]]; do
-    echo "Waiting for step analyze to start & exit..."
-    sleep 5
-done
+function bearerAuth() {
+  cat <<EOF > auth.json
+{"image-registry.openshift-image-registry.svc:5000":"Bearer $(echo "$(oc whoami -t)" | base64)"}
+EOF
+  kubectl delete cm registry-creds; kubectl create cm registry-creds --from-file=auth.json
 
-echo "########################################################"
-echo "Taskrun status reason: "
-#tkn taskrun describe buildpacks-phases-buildpacks -ojson | jq '.status.steps[] | select(.container == "step-analyze") | .terminated.reason'
-kubectl -n default logs buildpacks-phases-buildpacks-pod -c step-analyze
-echo "########################################################"
+  echo "############################################################################"
+  echo "Test using Bearer auth on ocp"
+  echo "Registry: image-registry.openshift-image-registry.svc:5000"
+  echo "Token: $(oc whoami -t)"
+  echo "############################################################################"
+}
 
-echo "#####################################################"
-echo "To get Tkn taskrun information, execute this command:"
-echo "tkn taskrun describe buildpacks-phases-buildpacks"
-echo "#####################################################"
+function cleanUp() {
+  kubectl delete -f https://raw.githubusercontent.com/redhat-buildpacks/testing/7615593bf80940f8410335decc9eccf6d9eeca18/k8s/tekton/buildpacks-phases.yml
+  kubectl apply -f https://raw.githubusercontent.com/redhat-buildpacks/testing/7615593bf80940f8410335decc9eccf6d9eeca18/k8s/tekton/buildpacks-phases.yml
+  kubectl delete PipelineRun/buildpacks-phases
+  kubectl delete pvc/ws-pvc
+}
+function checkResult() {
+    # Loop until the TaskRun exists
+    while [[ -z "$(kubectl get -n default taskrun buildpacks-phases-buildpacks -o json)" ]]; do
+        echo "Waiting for TaskRun buildpacks-phases-buildpacks to exist..."
+        sleep 10
+    done
 
+    # Loop until the status is not null
+    while [[ -z "$(tkn taskrun describe buildpacks-phases-buildpacks -ojson | jq '.status.steps[] | select(.container == "step-analyze" and has("terminated"))')" ]]; do
+        echo "Waiting for step analyze to start & exit..."
+        sleep 5
+    done
+
+    echo "########################################################"
+    echo "Taskrun status reason: "
+    #tkn taskrun describe buildpacks-phases-buildpacks -ojson | jq '.status.steps[] | select(.container == "step-analyze") | .terminated.reason'
+    kubectl -n default logs buildpacks-phases-buildpacks-pod -c step-analyze
+    echo "########################################################"
+
+    echo "#####################################################"
+    echo "To get Tkn taskrun information, execute this command:"
+    echo "tkn taskrun describe buildpacks-phases-buildpacks"
+    echo "#####################################################"
+}
+
+cleanUp
+generatePipelineRun
+
+basicAuth
+checkResult
+
+bearerAuth
+checkResult
