f2708201cf326f0287e73db5f0bc5ca74b290505
Charles Moulliard <cmoulliard@redhat.com>
2025-04-25 13:15:19 +0200

    Bump versions of Tekton, Shipwright, Quarkus and use renamed images (#66)
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

--- .github/testNodeHello.sh
+++ .github/testNodeHello.sh
@@ -4,6 +4,10 @@ CONTAINER_IMAGE=$2
 
 # Run container
 docker run -d --rm --name $CONTAINER_NAME -p 8080:8080 $CONTAINER_IMAGE
+if [ $? -ne 0 ]; then
+  echo "❌ Failed to start container $CONTAINER_NAME as image do not exist !"
+  exit 1
+fi
 
 # Wait until app is running
 while ! docker logs $CONTAINER_NAME | grep -q "Listening on port 8080";
