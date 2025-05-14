3db6ed0dc009d611738bad2c893050292c5e1d57
cmoulliard <cmoulliard@redhat.com>
2025-05-14 10:47:16 +0200

    WIP. Added the java buildpacks client
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

---
+++ .github/samples/build-me/README.md
@@ -0,0 +1,37 @@
+# Buildpack Builder config example
+
+Example of a java project able to build a Quarkus, Spring Boot, ... project
+using the DSL `BuildConfig.builder()`
+
+To use it, configure the following mandatory environment variables pointing to a project (example: [hello-quarkus](../hello-quarkus), [hello-spring](../hello-spring)) to be built as a container image
+
+```bash
+export PROJECT_PATH=<JAVA_PROJECT>
+# <IMAGE_REF> can be <IMAGE_NAME> without registry or a full registry reference with host, port(optional), path & tag
+export IMAGE_REF=<IMAGE_REF> 
+```
+
+If you plan to push your image to a registry, then set your registry credential using these variables:
+```bash
+export REGISTRY_USERNAME="<REGISTRY_USERNAME>"
+export REGISTRY_PASSWORD="<REGISTRY_PASSWORD>"
+export REGISTRY_SERVER="docker.io"
+```
+
+If you prefer that lifecycle access directly the registry internally, then set to true the following variable:
+```shell
+export USE_DAEMON=true
+```
+
+Execute this command in a terminal:
+```bash
+mvn compile exec:java
+```
+
+You can also pass the `BP_` or `CNB_` environment variables:
+```bash
+export BP_JVM_VERSION="21"
+export BP_MAVEN_BUILT_ARTIFACT="target/quarkus-app/lib/ target/quarkus-app/*.jar target/quarkus-app/app/ target/quarkus-app/quarkus"
+export CNB_LOG_LEVEL=debug
+etc
+```
---
+++ .github/samples/build-me/src/main/java/dev/snowdrop/BuildMe.java
@@ -0,0 +1,70 @@
+package dev.snowdrop;
+
+import java.io.File;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import dev.snowdrop.buildpack.*;
+import dev.snowdrop.buildpack.config.*;
+
+public class BuildMe {
+        
+    public static void main(String... args) {
+
+        System.setProperty("org.slf4j.simpleLogger.log.dev.snowdrop.buildpack","debug");
+        System.setProperty("org.slf4j.simpleLogger.log.dev.snowdrop.buildpack.docker","debug");
+        System.setProperty("org.slf4j.simpleLogger.log.dev.snowdrop.buildpack.lifecycle","debug");
+        System.setProperty("org.slf4j.simpleLogger.log.dev.snowdrop.buildpack.lifecycle.phases","debug");
+
+        String IMAGE_REF = Optional.ofNullable(System.getenv("IMAGE_REF"))
+            .orElseThrow(() -> new IllegalStateException("Missing env var: IMAGE_REF"));
+        String PROJECT_PATH = Optional.ofNullable(System.getenv("PROJECT_PATH"))
+            .orElseThrow(() -> new IllegalStateException("Missing env var: PROJECT_PATH"));
+        String USE_DAEMON = Optional.ofNullable(System.getenv("USE_DAEMON"))
+            .orElse("false");
+        String CNB_BUILDER_IMAGE = Optional.ofNullable(System.getenv("CNB_BUILDER_IMAGE"))
+            .orElse("paketobuildpacks/builder-ubi8-base:latest");
+
+        Map<String, String> envMap = System.getenv().entrySet().stream()
+            .filter(entry -> entry.getKey().startsWith("BP_") || entry.getKey().startsWith("CNB_"))
+            .collect(Collectors.toMap(
+                Map.Entry::getKey,
+                Map.Entry::getValue,
+                (oldValue, newValue) -> newValue,
+                HashMap::new
+            ));
+
+        List<RegistryAuthConfig> authInfo = new ArrayList<>();
+        if(System.getenv("REGISTRY_ADDRESS")!=null){
+          String registry = System.getenv("REGISTRY_SERVER");
+          String username = System.getenv("REGISTRY_USER");
+          String password = System.getenv("REGISTRY_PASS");
+          RegistryAuthConfig authConfig = RegistryAuthConfig.builder()
+                                                .withRegistryAddress(registry)
+                                                .withUsername(username)
+                                                .withPassword(password)
+                                                .build();
+          authInfo.add(authConfig);
+        }
+
+        int exitCode = BuildConfig.builder()
+            .withBuilderImage(new ImageReference(CNB_BUILDER_IMAGE))
+            .withOutputImage(new ImageReference(IMAGE_REF))
+            .withNewPlatformConfig()
+              .withEnvironment(envMap)
+            .endPlatformConfig()
+            .withNewDockerConfig()
+              .withAuthConfigs(authInfo)
+              .withUseDaemon(Boolean.parseBoolean(USE_DAEMON))
+            .endDockerConfig()
+            .withNewLogConfig()
+              .withLogger(new SystemLogger())
+              .withLogLevel("debug")
+            .and()
+            .addNewFileContentApplication(new File(PROJECT_PATH))
+            .build()
+            .getExitCode();
+
+        System.exit(exitCode);
+    }
+}
